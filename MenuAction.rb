a948d7a7994caf9396412c229b813ab92cec2c58
Deyvid <dmitkov@mail.uoguelph.ca>
2016-11-05 19:09:20 -0400

    adding all menu options and actions

---
+++ MenuAction.rb
@@ -0,0 +1,5 @@
+class MenuAction 
+  def activate()
+    #will be overwritten by child classes
+  end
+end
---
+++ MoveAction.rb
@@ -0,0 +1,10 @@
+class MoveAction < MenuAction 
+  @game
+  def initialize(game)
+    @game = game
+  end
+  
+  def activate(row_index, col_index, direction)
+    tile = @game.gameBoard[row_index][col_index]
+    return tile.hasPiece()
+  end
