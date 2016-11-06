ba30298012e9ce07c6b108a68bcd4a7cd2c01845
Deyvid <dmitkov@mail.uoguelph.ca>
2016-11-06 11:18:45 -0500

    doing some repo cleanup (forgot to stash before pulling)

--- menu.rb
+++
@@ -1,8 +0,0 @@
-class Menu
-  def initialize(name, description, prompt, options, prev_menu)
-    @name = name
-    @description = description
-    @prompt = prompt
-    @options = options
-  end
-end
