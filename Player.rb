b3c29c06a3d50ca18312ab9b30c126b61734e5f8
Deyvid <dmitkov@mail.uoguelph.ca>
2016-11-06 11:23:26 -0500

    still cleaning ffs

---
+++ Player.rb
@@ -0,0 +1,22 @@
+class Player
+  @piece_count = 0
+  def initialize(name)
+    @name = name
+  end
+
+  def takeTurn(Menu game_menu)
+    game_menu.display()
+    code = ''
+
+    print game_menu.prompt
+    code = handle_input(gets)
+      print game_menu.prompt
+  end
+
+  def move_piece(Tile from, Tile to)
+    to.place_piece(from.piece)
+  end
+  
+  def remove_piece
+    @piece_count--
+end
